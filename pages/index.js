503906d6187e67d6d1cf6186ebac4fc143e7f137
Nick Dandakis <nickdandakis@gmail.com>
2021-09-30 08:53:13 -0400

    Link out traits to OpenSea filters

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ import getRandomInt from '../utils/getRandomInt';
 
 const IMPERMANENT_DIGITAL_CONTRACT_ID = '0xa66f3bd98b4741bad68bcd7511163c6f855d2129';
 const CRYPTO_PUNKS_CONTRACT_ID = '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb';
+const OPENSEA_IMPERMANENT_DIGITAL_LIFECYCLE_FILTER_URL = 'https://opensea.io/collection/impermanent-digital?search[stringTraits][0][name]=Lifecycle%20Trait&search[stringTraits][0][values][0]=';
+const OPENSEA_IMPERMANENT_DIGITAL_WAVELENGTH_FILTER_URL = 'https://opensea.io/collection/impermanent-digital?search[stringTraits][0][name]=Wavelength&search[stringTraits][0][values][0]=';
+const OPENSEA_IMPERMANENT_DIGITAL_SIGNATURE_FILTER_URL = 'https://opensea.io/collection/impermanent-digital?search[stringTraits][0][name]=Signature Edition&search[stringTraits][0][values][0]=1%2F1';
 
 function IndexPage() {
   const router = useRouter();
@@ -127,13 +130,28 @@ function IndexPage() {
                   </a>
                 )}
                 {activeMetadataWavelength && (
-                  <span>{activeMetadataWavelength.value}</span>
+                  <a
+                    href={`${OPENSEA_IMPERMANENT_DIGITAL_WAVELENGTH_FILTER_URL}${activeMetadataWavelength.value}`}
+                    target="_blank"
+                  >
+                    {activeMetadataWavelength.value}
+                  </a>
                 )}
                 {activeMetadataLifecycle && (
-                  <span>L{activeMetadataLifecycle.value}</span>
+                  <a
+                    href={`${OPENSEA_IMPERMANENT_DIGITAL_WAVELENGTH_FILTER_URL}${activeMetadataLifecycle.value}`}
+                    target="_blank"
+                  >
+                    L{activeMetadataLifecycle.value}
+                  </a>
                 )}
                 {activeMetadataSignatureEdition && (
-                  <span>Signature Edition: {activeMetadataSignatureEdition.value}</span>
+                  <a
+                    href={OPENSEA_IMPERMANENT_DIGITAL_SIGNATURE_FILTER_URL}
+                    target="_blank"
+                  >
+                    Signature Edition: {activeMetadataSignatureEdition.value}
+                  </a>
                 )}
               </div>
             </footer>
